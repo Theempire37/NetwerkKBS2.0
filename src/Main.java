6adea6c5ccc219e96d38e59eafaab5440409dbd6
Theempire37 <44696817+Theempire37@users.noreply.github.com>
2019-05-08 13:09:44 +0200

    0.0.0.2eo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Test vanaf hiero");
         System.out.println("Hello World!");
         System.out.println("test");
     }
