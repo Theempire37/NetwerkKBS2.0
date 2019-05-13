dcb12e297f07313c46436854178e07a9ae3d71f2
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2019-05-13 12:42:39 +0200

    Version 0.1

---
+++ MonitoringKBS/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Scherm scherm = new Scherm();
+    }
+
+}
---
+++ MonitoringKBS/src/SkylabComponent.java
@@ -0,0 +1,11 @@
+public class SkylabComponent {
+
+    private boolean beschikbaar;
+    private double processorbelasting;
+    private double diskRuimte;
+    private int tijdTeller;
+
+
+
+
+}
