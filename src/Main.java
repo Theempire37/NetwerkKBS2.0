305d47a8e7a6ececf7a9ff43ee359990a5b94852
Theempire37 <44696817+Theempire37@users.noreply.github.com>
2019-05-13 12:26:39 +0200

    JDK 12.0.1 en Sleeptest

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test vanaf hiero");
+        TimeUnit.SECONDS.sleep(5);
         System.out.println("Hello World!");
         System.out.println("test");
     }
